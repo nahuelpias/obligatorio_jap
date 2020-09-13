e341e709969eed587817a7a46d59919d002d5661
Nahuel Pías <68924084+nahuelpias@users.noreply.github.com>
2020-09-13 09:17:22 -0300

    Funcionalidad info, relacionados y comentarios

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function (e) {
+
     var product = {};
 
 //Muestro Galería de imágenes
@@ -25,7 +25,7 @@ function showImagesGallery(array) {
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-
+document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
         if (resultObj.status === "ok")
             product = resultObj.data;
